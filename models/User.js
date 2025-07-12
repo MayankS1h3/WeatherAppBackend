30b9a4b8a0b7d98f5c362895992d599ecca9f92d
Mayank Sharma <102649566+MayankS1h3@users.noreply.github.com>
2025-07-13 02:24:20 +0530

    User model

---
+++ models/User.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        minLength: 1,
+        maxLength: 20,
+        required: [true, 'User name is required']
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        required: [true, 'Email is required'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minLength: 8
+    }
+});
+
+const User = new mongoose.model('User', userSchema);
+
+export default User;
